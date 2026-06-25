7c78181315e270883fcdf8fe2a022a6ee943ceb7
Yousef Hadhood <skillissuecmaster@gmail.com>
2026-06-26 01:13:22 +0300

    TODO: integrate this logic into the backend by friday night

--- scripts/wifi_esp.py
+++ scripts/wifi_esp.py
@@ -1,25 +1,33 @@
 import asyncio
-import time
 import wave
+from datetime import datetime
+
 import websockets
 
 ESP32_IP = "10.38.50.20"
 WS_URL = f"ws://{ESP32_IP}:81/"
+current_time = datetime.now().strftime("%H:%M")
+
 
 RECORD_SECONDS = 30
-SAMPLE_RATE = 8000
-OUTPUT_FILE = "esp32_buffered_audio.wav"
+SAMPLE_RATE = 8000  # do not change!
+OUTPUT_FILE = f"{current_time}_esp32_buffered_audio@{ESP32_IP}.wav"
+
 
 async def record_wave():
     print(f"Connecting to ESP32 at {WS_URL}...")
 
+    # We are tracking total BYTES now.
+    # 8000 samples/sec * 30 seconds = 240,000 samples.
+    # Each sample is a 16-bit integer (2 bytes).
+    # So we need exactly 480,000 bytes.
     TARGET_BYTES = SAMPLE_RATE * RECORD_SECONDS * 2
 
     try:
         async with websockets.connect(WS_URL, max_size=None) as websocket:
             print(f"Connected! Collecting {TARGET_BYTES} bytes of audio. Keep quiet...")
 
-            audio_frames = bytearray()  
+            audio_frames = bytearray()  # A highly efficient binary array
 
             while len(audio_frames) < TARGET_BYTES:
                 # Receive the chunk of 500 samples (1000 bytes)
